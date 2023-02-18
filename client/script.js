ef187e9b0e56efda89eaa9c313a04dd2927c4465
wzhgit <2165842569@qq.com>
2023-02-18 23:50:57 +0800

    update server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loading(botBlock)
 
   //fetch the bot response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chat-bot-a22j.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
